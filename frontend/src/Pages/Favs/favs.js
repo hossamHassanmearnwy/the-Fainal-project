8ca8bad00367950a07f797e92992172faddf05ad
Hussam Hassan <35435902+hossamHassanIbrahem@users.noreply.github.com>
2023-04-06 23:42:09 +0200

    not fond erorr
    component errors

--- frontend/src/Pages/Favs/favs.js
+++ frontend/src/Pages/Favs/favs.js
@@ -1,28 +1,53 @@
-import React from 'react'
-import "./fav.css"
-import Card from 'react-bootstrap/Card';
+import React from "react";
+import "./favs.css";
+import Card from "react-bootstrap/Card";
 
 export default function Fav() {
   return (
-    <div className='container'>
-     <div> <h3 className='hfav'> your favs  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="red" class="bi bi-heart-fill" viewBox="0 0 16 16" >
-  <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
-</svg> </h3></div>
-<br />
-<div className='container' id='fav'>
-<Card  className='col-4' id='favCard'>
-      <Card.Img variant="top" src="/1.jpg" className='cardImg' />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-       
-
-      </Card.Body>
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="red" className='myHeart' class="bi bi-heart-fill" viewBox="0 0 16 16" >
-  <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
-</svg>
-    </Card>
-
-</div>
+    <div className="container">
+      <div>
+        {" "}
+        <h3 className="hfav">
+          {" "}
+          your favs{" "}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="red"
+            class="bi bi-heart-fill"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"
+            />
+          </svg>{" "}
+        </h3>
+      </div>
+      <br />
+      <div className="container" id="fav">
+        <Card className="col-4" id="favCard">
+          <Card.Img variant="top" src="/1.jpg" className="cardImg" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+          </Card.Body>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="red"
+            className="myHeart"
+            class="bi bi-heart-fill"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"
+            />
+          </svg>
+        </Card>
+      </div>
     </div>
-  )
+  );
 }
--- frontend/src/Pages/NotFound/NotFound.js
+++ frontend/src/Pages/NotFound/NotFound.js
@@ -1,16 +1,21 @@
 import React from "react";
-import "./notfound.css";
+import "./NotFound.css";
 function NotFound() {
   return (
     <div className="my">
-        
       {/* page is not found
      <div> <button type="button" class="btn btn-warning">home</button></div> */}
- <div  className="container" class="mainbox">
-    <div class="err">4</div>
-    <i class="far fa-question-circle fa-spin"></i>
-    <div class="err2">4</div>
-    <div class="msg">Maybe this page moved? Got deleted? Is hiding out in quarantine? Never existed in the first place?<p>Let's go <a href="/Home">Home</a> and try from there.</p></div>
+      <div className="container" class="mainbox">
+        <div class="err">4</div>
+        <i class="far fa-question-circle fa-spin"></i>
+        <div class="err2">4</div>
+        <div class="msg">
+          Maybe this page moved? Got deleted? Is hiding out in quarantine? Never
+          existed in the first place?
+          <p>
+            Let's go <a href="/Home">Home</a> and try from there.
+          </p>
+        </div>
       </div>
     </div>
   );
