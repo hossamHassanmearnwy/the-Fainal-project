6d007eb27cbe6d9f191970383b2ccf981ac04db2
Hussam Hassan <35435902+hossamHassanIbrahem@users.noreply.github.com>
2023-04-08 02:08:00 +0200

    last commit in structure
    hossam rename names

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,125 +1,111 @@
-import React , {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import ProjFooter from "./Layouts/Footer/Footer";
-import InternalNav from './Layouts/InternalNav/InternalNav'
-import ProjectNav from './Layouts/Nav/ProjectNav'
-import Amazon from './component/Amazon/Amazon';
-import Cart from './component/cart/Cart';
-import './component/Amazon/amazon.css';
-
-
-
-
+import InternalNav from "./Layouts/InternalNav/InternalNav";
+import ProjectNav from "./Layouts/Nav/ProjectNav";
+import Amazon from "./Components/Amazon/Amazon";
+import Cart from "./Pages/cart/Cart";
+import "./Components/Amazon/amazon.css";
 
 //// F /////
 import UserAccount from "./Pages/user-account/UserAccount";
-// import Header from './component/header/header';
+// import Header from './Components/header/header';
 import { Route, Routes } from "react-router-dom";
-import MyAccount from './component/myAccount/myAccount';
-import MyOrders from './component/myOrders/myOrders';
-import MyWishList from './component/myWishList/myWishList';
-import AddressBook from './component/addressBook/addressBook';
-import AccountInformation from './component/accountInformation/accountInformation';
+import MyAccount from "./Components/myAccount/myAccount";
+import MyOrders from "./Components/myOrders/myOrders";
+import MyWishList from "./Components/myWishList/myWishList";
+import AddressBook from "./Components/addressBook/addressBook";
+import AccountInformation from "./Components/accountInformation/accountInformation";
 import Login from "./Pages/Login/login";
 import Register from "./Pages/Register/register";
 import ForgotPassword from "./Pages/ForgotPassword/forgotPassword";
-import Fav from './Pages/Favs/favs';
-import NotFound from './Pages/NotFound/NotFound';
+import Fav from "./Pages/Favs/favs";
+import NotFound from "./Pages/NotFound/NotFound";
+import Product from "./Components/product/product";
 //////////////////////
 
 function App() {
-
   const [show, setShow] = useState(true);
-	// const [cart , setCart] = useState([]);
-	let [cart, setCart] = useState(
-    JSON.parse(localStorage.getItem("cart")) || []   // local storge to get cart 
+  // const [cart , setCart] = useState([]);
+  let [cart, setCart] = useState(
+    JSON.parse(localStorage.getItem("cart")) || [] // local storge to get cart
   );
-	const [warning, setWarning] = useState(false);
+  const [warning, setWarning] = useState(false);
 
-
-	useEffect(() => {
-    localStorage.setItem("cart", JSON.stringify(cart));    // local storge to save cart in it
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart)); // local storge to save cart in it
 
     console.log(`Saved ${cart.length} items to localstorage`);
-  }, [cart]); 
+  }, [cart]);
 
+  const handleClick = (item) => {
+    let isPresent = false;
+    cart.forEach((product) => {
+      if (item.id === product.id) isPresent = true;
+      // console.log(product)
+    });
+    if (isPresent) {
+      setWarning(true);
+      setTimeout(() => {
+        setWarning(false);
+      }, 2000);
+      return;
+    }
+    setCart([...cart, item]);
+  };
 
+  const handleChange = (item, d) => {
+    let ind = -1;
+    cart.forEach((data, index) => {
+      if (data.id === item.id) ind = index;
+    });
+    const tempArr = cart;
 
-	const handleClick = (item)=>{
-		let isPresent = false;
-		cart.forEach((product)=>{
-			if (item.id === product.id)
-			isPresent = true;
-			// console.log(product)
-		})
-		if (isPresent){
-			setWarning(true);
-			setTimeout(()=>{
-				setWarning(false);
-			}, 2000);
-			return ;
-		}
-		setCart([...cart, item]);
-	}
+    tempArr[ind].amount += d;
 
-	const handleChange = (item, d) =>{
-		let ind = -1;
-		cart.forEach((data, index)=>{
-			if (data.id === item.id)
-				ind = index;
-		});
-		const tempArr = cart;
-
-		tempArr[ind].amount += d;
-		
-		if (tempArr[ind].amount === 0)
-			tempArr[ind].amount = 1;
-		setCart([...tempArr])
-	}
-
-
-
-
-
-
+    if (tempArr[ind].amount === 0) tempArr[ind].amount = 1;
+    setCart([...tempArr]);
+  };
 
   return (
     <>
       {/* <Header /> */}
 
+      <div className="App container-fluid bg-white">
+        <ProjectNav />
+        <InternalNav size={cart.length} setShow={setShow} />
 
-      <div className="App container-fluid bg-white" >
-      <ProjectNav />
-    <InternalNav size={cart.length} setShow={setShow}/>
-    
-      {
-			show ? <Amazon handleClick={handleClick} /> : <Cart cart={cart} setCart={setCart} handleChange={handleChange} setShow={setShow} />
-		}
-		{
-			warning && <div className='warning'>Item is already added to your cart</div>
-		}
-      <ProjFooter />
-    </div>
-
+        {show ? (
+          <Amazon handleClick={handleClick} />
+        ) : (
+          <Cart
+            cart={cart}
+            setCart={setCart}
+            handleChange={handleChange}
+            setShow={setShow}
+          />
+        )}
+        {warning && (
+          <div className="warning">Item is already added to your cart</div>
+        )}
+        <ProjFooter />
+      </div>
 
       <Routes>
-
         <Route path="/useraccount" element={<UserAccount />}>
-
           <Route index element={<MyAccount />} />
-          <Route path='myaccount' element={<MyAccount />} />
-          <Route path='myorders' element={<MyOrders />} />
-          <Route path='mywishlist' element={<MyWishList />} />
-          <Route path='addressbook' element={<AddressBook />} />
-          <Route path='accountinfo' element={<AccountInformation />} />
-          <Route path='Fav' element={<Fav />} />
-          <Route path='*' element={<NotFound/>} />
-      
-
+          <Route path="myaccount" element={<MyAccount />} />
+          <Route path="myorders" element={<MyOrders />} />
+          <Route path="mywishlist" element={<MyWishList />} />
+          <Route path="addressbook" element={<AddressBook />} />
+          <Route path="accountinfo" element={<AccountInformation />} />
+          <Route path="Fav" element={<Fav />} />
+          <Route path="product" element={<Product />} />
+          <Route path="*" element={<NotFound />} />
         </Route>
         <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/forgotpassword" element={<ForgotPassword />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgotpassword" element={<ForgotPassword />} />
       </Routes>
     </>
   );
---
+++ frontend/src/Components/product/product.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Products.css";
+
+const Product = () => {
+  return (
+    <div className="card">
+      <div className="imgBox">
+        <img
+          src="https://www.corsair.com/corsairmedia/sys_master/productcontent/CH-9300011-NA-M65_PRO_RGB_BLK_04.png"
+          alt="mouse corsair"
+          className="mouse"
+        />
+      </div>
+      <div className="contentBox">
+        <h3>Mouse Corsair M65</h3>
+        <h2 className="price">
+          61.<small>98</small> €
+        </h2>
+        <a href="w" className="buy">
+          Buy Now
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Product;
