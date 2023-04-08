a85a05a9b57348c45060ecde05bd6af00de6a31e
Hussam Hassan <35435902+hossamHassanIbrahem@users.noreply.github.com>
2023-04-09 01:41:15 +0200

    connection with out link
    adeed

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const mongoose =require("mongoose"); ;
+const port = process.env.PORT || 3000;
+require('dotenv').config()
+console.log(`process.env`)
+// const dbConnection = require("./db/mongoos");
+// dbConnection();
 
 app.get("/", (req, res) => res.send("Hello team 5!"));
 app.listen(port, () => console.log(`app listening on port ${port}!`));
