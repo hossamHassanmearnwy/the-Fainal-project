77902f4ebe5c87af4742ebdb2ace1ccffe015cd1
Hussam Hassan <35435902+hossamHassanIbrahem@users.noreply.github.com>
2023-04-09 02:15:59 +0200

    last
    last

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
-const mongoose =require("mongoose"); ;
+const mongoose = require("mongoose");
 const port = process.env.PORT || 3000;
-require('dotenv').config()
-console.log(`process.env`)
-// const dbConnection = require("./db/mongoos");
-// dbConnection();
+require("dotenv").config();
+console.log(`process.env`);
+const dbConnection = require("./DB/connection");
+dbConnection();
 
 app.get("/", (req, res) => res.send("Hello team 5!"));
 app.listen(port, () => console.log(`app listening on port ${port}!`));
