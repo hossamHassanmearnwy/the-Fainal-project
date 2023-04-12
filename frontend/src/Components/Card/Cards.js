1d5996c595c41b792ee422105512ea07758d21d4
Hussam Hassan <35435902+hossamHassanIbrahem@users.noreply.github.com>
2023-04-12 05:54:33 +0200

    do some change
    do some change

--- frontend/src/Components/Card/Cards.js
+++ frontend/src/Components/Card/Cards.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React from 'react';
 import '../Card/cards.css';
 import { BsFillCartFill } from "react-icons/bs";
@@ -6,8 +7,28 @@ import { FaRegHeart } from "react-icons/fa";
 const Cards = ({item, handleClick}) => {
     const {title, desc, price, img} = item;
     // console.log(item)
-  return (
+=======
+import React from "react";
 
+import { BsFillCartFill } from "react-icons/bs";
+import { FaRegHeart } from "react-icons/fa";
+
+import "../Card/cards.css";
+import { useTranslation } from "react-i18next";
+
+const Cards = ({ item, handleClick }) => {
+  const { t, i18n } = useTranslation();
+  document.body.dir = i18n.dir();
+  const { title, desc, price, img } = item;
+  // console.log(item)
+>>>>>>> Stashed changes
+  return (
+    <>
+      <div className="cardPro my-3 border border-warning">
+        <div className="imgBox">
+          <img className="mouse" src={img} alt="Image" />
+
+<<<<<<< Updated upstream
 
     // <div className="cards my-4 col-lg-4 col-sm-12">
     //     <div className="image_box">
@@ -38,19 +59,53 @@ const Cards = ({item, handleClick}) => {
       <span className= 'p-3 circle bg-warning'  style={{borderRadius:"50%"}} onClick={()=>handleClick(item)} role='button'>
       <BsFillCartFill className='text-white fs-4' role='button'> </BsFillCartFill>
       </span>
+=======
+          <div className="cards">
+            <div className="image_box">
+              <img src={img} alt="Image" />
+            </div>
+            <div className="details">
+              <p>{title}</p>
+              <p>{desc}</p>
+              <p>
+                {t("Price")} - {price}
+              </p>
+              <button onClick={() => handleClick(item)}>
+                {t("Add to Cart")}
+              </button>
+            </div>
+          </div>
+          <div className="contentBox">
+            <h2 className="text-dark mt-3 fs-4 fw-bold">{title}</h2>
+            <div className="row">
+              <h5 className=" col-lg-6 col-sm-12 text-dark d-flex align-items-end">
+                Price - {price}
+              </h5>
+              <div className="col-lg-6 col-sm-12 mt-4">
+                <span
+                  className="p-3 circle bg-warning"
+                  style={{ borderRadius: "50%" }}
+                  onClick={() => handleClick(item)}
+                  role="button"
+                >
+                  <BsFillCartFill className="text-white fs-4" role="button">
+                    {" "}
+                  </BsFillCartFill>
+                </span>
+              </div>
+            </div>
+            <div className="mt-4">
+              <p className="buy border-top">
+                <FaRegHeart className="me-3 fs-4 text-danger"></FaRegHeart> add
+                to wishlist
+              </p>
+            </div>
+          </div>
+        </div>
+>>>>>>> Stashed changes
       </div>
-      </div>
-      <div className='mt-4'>
+    </>
+  );
+};
 
-      <p className='buy border-top'><FaRegHeart className='me-3 fs-4 text-danger'></FaRegHeart>  add to wishlist</p>
-
-      </div>
-      </div>
-  </div>
-
-
-
-  )
-}
-
-export default Cards
+export default Cards;
