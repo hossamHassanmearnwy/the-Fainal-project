941459a7c725be6a5c17f72b74e3abf3bd286652
Fatma-Mahmoud-55 <113183957+Fatma-Mahmoud-55@users.noreply.github.com>
2023-04-14 21:24:36 +0200

    Offer Slider

---
+++ frontend/src/Pages/Checkout/Checkout.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./CheckoutStyle.css"
+
+export default function Checkout() {
+  return (
+    <>
+    <div className='row'>
+        <div className='col-4'>dddddddddddddddddddd</div>
+        <div className='col-4'></div>
+        <div className='col-4'></div>
+
+
+
+    </div>
+    
+    
+    
+    
+    
+    </>
+  )
+}
--- frontend/src/Pages/Home/home.js
+++ frontend/src/Pages/Home/home.js
@@ -3,9 +3,51 @@ import Amazon from '../../Components/Amazon/Amazon'
 import MainSideBar from './../../Components/MainSideBar/MainSideBar';
 import Slider from './../../Components/Slider/Slider';
 import Cart from "../../Pages/cart/Cart";
+import OffersSlider from "../../Components/offersSlider/OffersSlider";
 
 export default function Home() {
-   const [warning, setWarning] = useState(false);
+  //  const [warning, setWarning] = useState(false);
+  const [show, setShow] = useState(true);
+  // const [cart , setCart] = useState([]);
+  let [cart, setCart] = useState(
+    JSON.parse(localStorage.getItem("cart")) || [] // local storge to get cart
+  );
+  const [warning, setWarning] = useState(false);
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart)); // local storge to save cart in it
+
+    console.log(`Saved ${cart.length} items to localstorage`);
+  }, [cart]);
+
+  const handleClick = (item) => {
+    let isPresent = false;
+    cart.forEach((product) => {
+      if (item.id === product.id) isPresent = true;
+      // console.log(product)
+    });
+    if (isPresent) {
+      setWarning(true);
+      setTimeout(() => {
+        setWarning(false);
+      }, 2000);
+      return;
+    }
+    setCart([...cart, item]);
+  };
+
+  const handleChange = (item, d) => {
+    let ind = -1;
+    cart.forEach((data, index) => {
+      if (data.id === item.id) ind = index;
+    });
+    const tempArr = cart;
+
+    tempArr[ind].amount += d;
+
+    if (tempArr[ind].amount === 0) tempArr[ind].amount = 1;
+    setCart([...tempArr]);
+  };
  
   return (<>
 
@@ -23,8 +65,16 @@ export default function Home() {
   </div>
   </div>
   
- 
+    {/* <Cart
+      cart={cart}
+      setCart={setCart}
+      handleChange={handleChange}
+      setShow={setShow}
+      path='/cart'
+    />  */}
     <Amazon />
+    <OffersSlider/>
+
     
     {warning && (
       <div className="warning">Item is already added to your cart</div>
