ef45d6932628af1efe29561fc38e31da415183f8
Hussam Hassan <35435902+hossamHassanIbrahem@users.noreply.github.com>
2023-04-17 00:06:32 +0200

    axios config wating back end

--- frontend/src/Components/offersSlider/OffersSlider.js
+++ frontend/src/Components/offersSlider/OffersSlider.js
@@ -1,70 +1,69 @@
-import React from 'react'
-import Sec from './../styleSec/style';
-import "./OffersSliderStyle.css"
+import React from "react";
+import Sec from "./../styleSec/style";
+import "./OffersSliderStyle.css";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Cards from './../Card/Cards';
-import list from '../../data';
-import Card from 'react-bootstrap/Card';
-import ListGroup from 'react-bootstrap/ListGroup';
-import offersList from './../../offersData';
-import { Icon, Badge } from "rsuite";
-import { BsCartCheckFill  } from "react-icons/bs";
-import { useTranslation } from 'react-i18next';
-
-
+// import Cards from './../Card/Cards';
+// import list from '../../data';
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
+import offersList from "./../../offersData";
+// import { Icon, Badge } from "rsuite";
+import { BsCartCheckFill } from "react-icons/bs";
+import { useTranslation } from "react-i18next";
 
 export default function OffersSlider() {
-    const {t, i18n} = useTranslation();
-    document.body.dir = i18n.dir();
-    const settings = {
-        dots: true,
-        infinite: true,
-        slidesToShow: 5,
-        slidesToScroll: 1,
-        autoplay: true,
-        speed: 3000,
-        autoplaySpeed: 3000,
-        cssEase: "linear"
-    };
-    return (<>
-        <div className='container '>
-            <Sec header='Deals of The Day' />
-            <hr />
-                <Slider {...settings}>
-                    
-                        {offersList.map((item)=>(
-                        <div className='col-3'>
-                                <Card  className='offerCard'>
-                                    <div class="badge-overlay">
-                                        {/* <!-- Change Badge Position, Color, Text here--> */}
-                                        <span class="top-left badge orange">{t('Sale')}</span>
-                                    </div>
-                                    
-                                    <Card.Img variant="top" src={item.img} className='slide-img'/>
-                                    <Card.Body>
-                                        <Card.Title>{item.title}</Card.Title>
-                                    {/* <Badge content="NEW"></Badge> */}
-                                        <Card.Text>
-                                        {item.desc}
-                                        </Card.Text>
-                                    </Card.Body>
-                                    <ListGroup className="list-group-flush">
-                                        <ListGroup.Item>{t('Brand')} : {item.brand}</ListGroup.Item>
-                                        <ListGroup.Item>{t('Price')} : {item.price}</ListGroup.Item>
-                                    </ListGroup>
-                                    <Card.Body>
-                                        <Card.Link href="cart"><BsCartCheckFill className='cartIcon' /> {t('Add to Cart')}</Card.Link>
-                                        
-                                    </Card.Body>
-                                </Card>
-                            
-                            </div>
-                        ))}
-                    </Slider>
-                
-        </div>
+  const { t, i18n } = useTranslation();
+  document.body.dir = i18n.dir();
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 3000,
+    autoplaySpeed: 3000,
+    cssEase: "linear",
+  };
+  return (
+    <>
+      <div className="container ">
+        <Sec header="Deals of The Day" />
+        <hr />
+        <Slider {...settings}>
+          {offersList.map((item) => (
+            <div className="col-3">
+              <Card className="offerCard">
+                <div class="badge-overlay">
+                  {/* <!-- Change Badge Position, Color, Text here--> */}
+                  <span class="top-left badge orange">{t("Sale")}</span>
+                </div>
+
+                <Card.Img variant="top" src={item.img} className="slide-img" />
+                <Card.Body>
+                  <Card.Title>{item.title}</Card.Title>
+                  {/* <Badge content="NEW"></Badge> */}
+                  <Card.Text>{item.desc}</Card.Text>
+                </Card.Body>
+                <ListGroup className="list-group-flush">
+                  <ListGroup.Item>
+                    {t("Brand")} : {item.brand}
+                  </ListGroup.Item>
+                  <ListGroup.Item>
+                    {t("Price")} : {item.price}
+                  </ListGroup.Item>
+                </ListGroup>
+                <Card.Body>
+                  <Card.Link href="cart">
+                    <BsCartCheckFill className="cartIcon" /> {t("Add to Cart")}
+                  </Card.Link>
+                </Card.Body>
+              </Card>
+            </div>
+          ))}
+        </Slider>
+      </div>
     </>
-    )
+  );
 }
