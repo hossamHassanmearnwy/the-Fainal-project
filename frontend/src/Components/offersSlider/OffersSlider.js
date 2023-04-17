1ebf2055d8da5dda47e2e1724e8abb1247b0e8f4
Fatma-Mahmoud-55 <113183957+Fatma-Mahmoud-55@users.noreply.github.com>
2023-04-17 06:13:06 +0200

    offer slider responsive

--- frontend/src/Components/offersSlider/OffersSlider.js
+++ frontend/src/Components/offersSlider/OffersSlider.js
@@ -1,21 +1,18 @@
-import React from "react";
+import { React, useState,useEffect } from "react";
 import Sec from "./../styleSec/style";
 import "./OffersSliderStyle.css";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-// import Cards from './../Card/Cards';
-// import list from '../../data';
 import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
 import offersList from "./../../offersData";
-// import { Icon, Badge } from "rsuite";
 import { BsCartCheckFill } from "react-icons/bs";
 import { useTranslation } from "react-i18next";
 
 
 export default function OffersSlider({ handleClick }) {
-  const settings = {
+  const settings={
     dots: true,
     infinite: true,
     slidesToShow: 5,
@@ -24,13 +21,41 @@ export default function OffersSlider({ handleClick }) {
     speed: 3000,
     autoplaySpeed: 3000,
     cssEase: "linear"
-  };
+  }
+
+  const [windowSize, setWindowSize] = useState(
+    window.innerWidth,
+  );
+  useEffect(() => {
+    const handleWindowResize = () => {
+      setWindowSize(window.innerWidth);
+    };
+    window.addEventListener('resize', handleWindowResize);
+  }, []);
+  
+  switch (windowSize){
+        case 1000:
+        settings.slidesToShow=3;
+        break;
+        case 800:
+        settings.slidesToShow =2;
+        break;
+        case 600:
+        case 500:
+        settings.slidesToShow=1;
+        break;
+      }
+    
   return (<>
-    <div className='container '>
+    {/* <div>
+      <h2>Width: {windowSize}</h2>
+
+      <h2>Height: {windowSize}</h2>
+    </div> */}
+    <div className='container ' >
       <Sec header='Deals of The Day' />
       <hr />
       <Slider {...settings}>
-
         {offersList.map((item) => (
           <div className='col-3'>
             <Card className='offerCard'>
@@ -42,7 +67,6 @@ export default function OffersSlider({ handleClick }) {
               <Card.Img variant="top" src={item.img} className='slide-img' />
               <Card.Body>
                 <Card.Title>{item.title}</Card.Title>
-                {/* <Badge content="NEW"></Badge> */}
                 <Card.Text>
                   {item.desc}
                 </Card.Text>
@@ -62,6 +86,8 @@ export default function OffersSlider({ handleClick }) {
       </Slider>
 
     </div>
+    
   </>
+  
   );
 }
