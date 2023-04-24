a75ae9a00619910fb1db570483e00a280266965d
Hussam Hassan <35435902+hossamHassanIbrahem@users.noreply.github.com>
2023-04-24 21:57:50 +0200

    some of style and repair error done
    some of style and repair error done

--- frontend/src/Components/styleSec/bestCard.js
+++ frontend/src/Components/styleSec/bestCard.js
@@ -1,31 +1,38 @@
-import React from 'react';
+import React from "react";
 import { BsFillCartFill } from "react-icons/bs";
-import './style.css';
+import "./style.css";
 
-const BestCard = ({best, handleClick}) => {
-    const {title, price, img} = best;
-    // console.log(best)
+const BestCard = ({ best, handleClick }) => {
+  const { title, price, img } = best;
+  // console.log(best)
   return (
+    <div className="bestc p-3 mb-3 ms-1 border-end">
+      <div className="d-flex direction-row">
+        <div
+          className="col-lg-4 me-3"
+          style={{ width: "85px", height: "100px" }}
+        >
+          <img src={img} alt="#" style={{ width: "100%", height: "100%" }} />
+        </div>
+        <div className="col-lg-8 ms-4">
+          <p className="mb-3 fw-bold text-primary text-start">{title}</p>
 
-
-    <div className='bestc p-3 mb-3 ms-1 border-end'>
-    <div className="d-flex direction-row">
-    <div className="col-lg-4 me-3" style={{width:"85px" , height:"100px"}}>
-    <img src={img} alt="#" style={{width:"100%" , height:"100%"}}/>
+          <p className=" text-dark text-start ">${price}</p>
+          <span
+            className="p-2 circle bg-warning"
+            style={{ borderRadius: "50%" }}
+            onClick={() => handleClick(best)}
+            role="button"
+          >
+            <BsFillCartFill
+              className="text-white text-end fs-5"
+              role="button"
+            ></BsFillCartFill>
+          </span>
+        </div>
+      </div>
     </div>
-    <div className="col-lg-8 ms-4">
-    <p className="mb-3 fw-bold text-primary text-start">{title}</p>
-
-    <p className=" text-dark text-start ">${price}</p>
-    <span className= 'p-2 circle bg-warning'  style={{borderRadius:"50%"}} onClick={()=>handleClick(best)} role='button'>
-    <BsFillCartFill className='text-white text-end fs-5' role='button'></BsFillCartFill>
-    </span>
-
-    </div>
-    </div>
-    </div>
-
-  )
-}
+  );
+};
 
 export default BestCard;
