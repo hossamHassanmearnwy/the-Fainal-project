93614f61cfbaf7f077b62007e2d3a7f80c9fab88
marina samy <marina.basalios@gmail.com>
2023-04-25 20:20:29 +0200

    add component handle routing & not found page

--- admin/src/app/app.module.ts
+++ admin/src/app/app.module.ts
@@ -5,12 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SideNavComponent } from './components/side-nav/side-nav.component';
 import { HeaderComponent } from './components/header/header.component';
+import { ProductsComponent } from './components/products/products.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { OrdersComponent } from './components/orders/orders.component';
+import { UsersComponent } from './components/users/users.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SideNavComponent,
-    HeaderComponent
+    HeaderComponent,
+    ProductsComponent,
+    CategoriesComponent,
+    OrdersComponent,
+    UsersComponent,
+    DashboardComponent,
+    NotfoundComponent,
   ],
   imports: [
     BrowserModule,
--- admin/src/app/components/side-nav/side-nav.component.ts
+++ admin/src/app/components/side-nav/side-nav.component.ts
@@ -13,26 +13,31 @@ export class SideNavComponent implements OnInit{
     {
       number: '1',
       name: 'Dashboard',
+      link:'/dashboard',
       icon: "fa-solid fa-chart-pie",
     },
     {
       number: '2',
       name: 'Products',
+      link:'/product',
       icon: "fa-solid fa-mobile-screen-button",
     },
     {
       number: '3',
       name: 'Categories',
+      link:'/category',
       icon: "fa-solid fa-box",
     },
     {
       number: '4',
       name: 'Users',
+      link:'/user',
       icon: "fa-solid fa-users",
     },
     {
       number: '5',
       name: 'Orders',
+      link:'/order',
       icon: "fa-solid fa-cart-shopping",
     }
   ]
